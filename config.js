1b22a060858f41d121eb37b5624133029c4c85a1
Bishal90hn <bishalchowdhury10089@gmail.com>
2025-04-15 06:44:44 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  botToken: '7980947590:AAFrmaCIFajzk0NjR-aZE7PwuIGrs45WGGo',
+  channelId: '@DeelDhamaka3',
+  proxy: {
+    host: '13.234.24.116',
+    port: 80,
+    protocol: 'http'
+  }
+};
